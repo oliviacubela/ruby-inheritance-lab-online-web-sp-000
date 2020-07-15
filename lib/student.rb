5862b8d3779a87dc7e52fbbead3bc12a1a989d91
Olivia Cubela <oliviacubela@gmail.com>
2020-07-15 18:40:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,13 +1,4 @@
 require_relative "./user.rb"
 class Student < User
-  attr_accessor :knowledge
-  @@knowledge = []
 
-  def initialize
-    @@knowledge = []
-  end
-
-  def self.knowledge
-    @@knowledge
-  end
 end
